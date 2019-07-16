e3f97f722fc239ddac39fc3679e8c61d2f7d67f6
ttt <1489272013@qq.com>
2019-07-16 13:30:47 -0700

    Full Annotation

--- tutian/lab1/3_2_two_processors/i2cm/i2cm.ino
+++ tutian/lab1/3_2_two_processors/i2cm/i2cm.ino
@@ -1,35 +1,48 @@
-#include <Wire.h>
+/*
+  Module Name:
+    i2cm.ino
+  
+  Description:
+    This program sets up a I2C Bus on a MASTER board to send command to a SLVAE.
+    A button is attached onto the board and the two board are connected through A4 and A5.
+    Serial Port is opend JUST FOR DEBUGGING.
 
-const int BUTTON_PIN = 9;
-int press_count = 0;
+  Author:
+    Shihao Piao, Ziliang Guo, Tutian Tang
+
+  Rev. 1 14 July 2019
+ */
+
+#include <Wire.h>             // library for I2C Bus
+
+const int BUTTON_PIN = 9;     // define where the button is conncted to
+int press_count = 0;          // count the times of the button being pressed
 
 void setup()
 {
-  Wire.begin();       // join i2c bus (address optional for master)
-  Serial.begin(9600); // start serial for output
-  pinMode(BUTTON_PIN, INPUT);
+  Wire.begin();               // join i2c bus (address optional for master)
+  Serial.begin(9600);         // start serial for output
+  pinMode(BUTTON_PIN, INPUT); // initialize the Button
 }
-
-byte x = 0;
 
 void loop()
 {
-  if (digitalRead(BUTTON_PIN))
+  if (digitalRead(BUTTON_PIN)) // If the button is pressed
   {
     Wire.beginTransmission(4); // transmit to device #4
-    press_count++;
-    if (press_count % 2)
+    press_count++;             // button pressed, counter ++
+    if (press_count % 2)       // if the counter is even, turn on the LED
     {
-      Wire.write('O');
-      Serial.println("ON");
+      Wire.write('O');         // send the command to the SLVAE
+      Serial.println("ON");    // FOR DEBUGGING ONLY
     }
-    else
+    else                       // if the counter is even, turn off the LED
     {
-      Wire.write('F');
-      Serial.println("OFF");
+      Wire.write('F');         //send the command to the SLVAE
+      Serial.println("OFF");   // FOR DEBUGGING ONLY
     }
-    Wire.endTransmission();
-    delay(200);
+    Wire.endTransmission();    // ends the transmission
+    delay(200);                // wait for the slave to make the move
   }
-  delay(50);
+  delay(50);                   // wait for a few millionseconds to save power
 }
--- tutian/lab1/3_2_two_processors/i2cs/i2cs.ino
+++ tutian/lab1/3_2_two_processors/i2cs/i2cs.ino
@@ -1,29 +1,48 @@
+/*
+  Module Name:
+    i2cs.ino
+  
+  Description:
+    This program sets up a I2C Bus on a SLAVE board to receive the command from MASTER.
+    A LED is attached onto the board and the two board are connected through A4 and A5.
+    Serial Port is opend JUST FOR DEBUGGING.
+
+  Author:
+    Shihao Piao, Ziliang Guo, Tutian Tang
+
+  Rev. 1 14 July 2019
+ */
+
 #include <Wire.h>
 
-const int LED_PIN = LED_BUILTIN;
+const int LED_PIN = 9;
 
 void setup()
 {
   Wire.begin(4);                // join i2c bus with address #4
   Wire.onReceive(receiveEvent); // register event
-  Serial.begin(9600);           // start serial for output
-  pinMode(LED_PIN, OUTPUT);
+  Serial.begin(9600);           // start serial for DEBUG ONLY
+  pinMode(LED_PIN, OUTPUT);     // initialize the LED
 }
+
 void loop()
 {
-  delay(50);
+  delay(50); // Save power
 }
+
 // function that executes whenever data is received from master
 // this function is registered as an event, see setup()
 void receiveEvent(int howMany)
 {
-  char c = Wire.read(); // receive byte as a character
-  if ('O' == c)
+  char c = Wire.read();          // receive byte as a character
+  if ('O' == c)                  // 'O' for ON and otherwise OFF
   {
-    digitalWrite(LED_PIN, HIGH);
+    digitalWrite(LED_PIN, HIGH); // TURN ON the LED
+    Serial.println("ON");        // FOR DEBUGGING
   }
   else
   {
-    digitalWrite(LED_PIN, LOW);
+    digitalWrite(LED_PIN, LOW); // TURN OFF the LED
+    Serial.println("OFF");      // FOR DEBUGGING
   }
 }
