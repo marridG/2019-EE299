d29eea6a21b3374673713217f0ade4d36ccc3211
marridG <1687450542@qq.com>
2019-07-19 08:58:41 -0700

    Lab 1&2

--- Lab 1/GZL/#2 1_name.ino
+++ Lab 1/GZL/#2 1_name.ino
@@ -1,3 +1,37 @@
+/*
+Module Name:
+  #2 1_name.ino
+
+Description:
+  Print the first name of each group member on the LCD display.
+  Two names are on the first row, while the remaining one on
+  the second.
+  Blink the text in an interval of 500ms.
+
+Code source:
+  Examples -> LiquidCrystal -> HelloWorld
+  LiquidCrystal Library - display() and noDisplay()
+
+With reference to:
+  https://www.arduino.cc/en/Reference/LiquidCrystal
+  Electronic Bricks Starter Kit Cookbook v1.3
+
+Modifications applied:
+  (1) We used BUS 1 to connect the LCD display, so the pin numbers
+  are modified to "lcd(2, 3, 4, 5, 6, 7, 8);"
+  (2) use setCursor() so as to implement the name-printing function
+
+Public Interface:
+  Blink the first name of eahc group member on the LCD display in 
+  an interval of 500ms.
+
+Side Effects:
+  No
+
+Authors:
+  Shihao Piao, Tutian Tang, Ziliang Guo
+Rev. 0 - 8 Jul
+*/
 // include the library code:
 #include <LiquidCrystal.h>
 
@@ -6,15 +40,26 @@
 const int rs = 2, en = 3, d4 = 4, d5 = 5, d6 = 6, d7 = 7, d8 = 8;
 LiquidCrystal lcd(rs, en, d4, d5, d6, d7, d8);
 
+/**
+ * [setup description]
+ *   setup all the hardware pin numbers and initialize the displayed
+ *   text of LCD
+ * @return {NONE}
+ */
 void setup() {
   // set up the LCD's number of columns and rows:
   lcd.begin(16, 2);
   // Print a message to the LCD.
-  lcd.print("Ziliang Shihao");
+  lcd.print("Shihao Tutian");
   lcd.setCursor(0,1);
-  lcd.print("Tutian");
+  lcd.print("Ziliang");
 }
 
+/**
+ * [loop description]
+ *   continuously blink the displayed text.
+ * @return {NONE}
+ */
 void loop() {
   // Turn off the display:
   lcd.noDisplay();
--- Lab 1/GZL/#2 2_move x.ino
+++ Lab 1/GZL/#2 2_move x.ino
@@ -1,11 +1,48 @@
+/*
+Module Name:
+  #2 2_move x.ino
+
+Description:
+  Show a "x" on the LCD display. There are two buttons connecting
+  to the board, called the "left" and the "right". After pressing
+  the left one, the "x" will be moved to the left by one column;
+  to the right by one column for the right, respectively.
+  If the "x" moves to the right from the very right side, it will
+  return to the very left side and viceversa.
+
+Code source:
+  Examples -> LiquidCrystal -> HelloWorld
+  LiquidCrystal Library - display() and noDisplay()
+
+With reference to:
+  https://www.arduino.cc/en/Reference/LiquidCrystal
+  Electronic Bricks Starter Kit Cookbook v1.3
+
+Modifications applied:
+  (1) We used BUS 1 to connect the LCD display, so the pin numbers
+  are modified to "lcd(2, 3, 4, 5, 6, 7, 8);"
+  (2) use setCursor() so as to implement the location changes of "x"
+
+Public Interface:
+  Show "x" on the LCD display.
+  Respond to user's pressing different buttons.
+
+Side Effects:
+  No. The locations of "x" change quite fast and smoothly!
+
+Authors:
+  Shihao Piao, Tutian Tang, Ziliang Guo
+Rev. 0 - 8 Jul
+*/
+
 // include the library code:
 #include <LiquidCrystal.h>
 
-const int MAX_WIDTH=16;
-const int DELAY_INTERVAL=50;
-int button_left=9;
-int button_right=11;
-int character_loc=0;
+const int MAX_WIDTH=16;         // the width of the LCD display
+// const int DELAY_INTERVAL=50;    // delay to wait for status change
+int button_left=9;              // the pin number of the left button
+int button_right=11;            // the pin number of the right button
+int character_loc=0;            // the number of column where "x" locates
 
 // initialize the library by associating any needed LCD interface pin
 // with the arduino pin number it is connected to
@@ -14,10 +51,12 @@ LiquidCrystal lcd(rs, en, d4, d5, d6, d7, d8);
 
 void setup()
 {
+  // setup the pin numbers of the two buttons
   pinMode(button_left, INPUT);
   pinMode(button_right, INPUT);
-  // set up the LC  D's number of columns and rows:
+  // set up the LC  D's number of columns and rows
   lcd.begin(MAX_WIDTH, 2);
+  // initialize the cursor location and the output "x"
   lcd.setCursor(0,0);
   lcd.print("x");
   // Serial.begin(9600);
@@ -25,51 +64,63 @@ void setup()
 
 void loop()
 {
-//  lcd.display();
-//  delay(500);
+/* Approach One
+  lcd.display();
+  delay(500);
+*/
+  // to indicate whether a button is pressed. True for yes; false for not.
   bool status_change=false;
-  // if(digitalRead(button_left))
-  // {
-  //     delay(DELAY_INTERVAL);
-  //     if(!digitalRead(button_left))
-  //     {
-  //       character_loc+=1;
-  //     }
-  //     status_change=true;
-  // }
-  // if(digitalRead(button_right))
-  // {
-  //     delay(DELAY_INTERVAL);
-  //     if(!digitalRead(button_right))
-  //     {
-  //       character_loc-=1;
-  //     }
-  //     status_change=true;
-  // }
-  if(digitalRead(button_left))
+/*if(digitalRead(button_left))
+  {
+    delay(DELAY_INTERVAL);
+    if(!digitalRead(button_left))
+    {
+      character_loc+=1;
+    }
+    status_change=true;
+  }
+  if(digitalRead(button_right))
+  {
+    delay(DELAY_INTERVAL);
+    if(!digitalRead(button_right))
+    {
+      character_loc-=1;
+    }
+    status_change=true;
+  }
+*/
+  if(digitalRead(button_left))        // the left button is of a high voltage, thus pressed
   {
     character_loc++;
     status_change=true;
     // Serial.println("left");
   }
-  else if (digitalRead(button_right))
+  else if (digitalRead(button_right)) // the right button is of a high voltage, thus pressed
   {
     character_loc--;
     status_change=true;
     // Serial.println("right");
   }
 
+  // if a button is pressed, change the output location of the "x"
   if(status_change)
   {
+    // check whether the location overflows
+    // if larger than maximum, set to 0; if less than minimum, set to maximum
     if(character_loc>=MAX_WIDTH)
       character_loc-=16;
     else if(character_loc<0)
       character_loc+=16;
 
+    // clear the previously printed "x"
     lcd.clear();
+
+    // set the cursor to the new location and print "x"
     lcd.setCursor(character_loc,0);
     lcd.print("x");
+
     delay(500);
+    // set the status back to that indicating no button is pressed
     status_change=false;
   }
 
