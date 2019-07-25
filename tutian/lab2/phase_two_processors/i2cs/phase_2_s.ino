745b8449b7aed5af0c2fce29b068e316092dbc7d
ttt <1489272013@qq.com>
2019-07-25 14:37:23 -0700

    Add comments

--- tutian/lab2/phase_two_processors/i2cs/phase_2_s.ino
+++ tutian/lab2/phase_two_processors/i2cs/phase_2_s.ino
@@ -1,31 +1,53 @@
-#include <LiquidCrystal.h>
-#include <Wire.h>
+/*
+  Module Name:
+    phase_2_s.ino
+  
+  Description:
+    This program sets up a I2C Bus on a SLAVE board to receive the command from MASTER.
+    A LCD panel is attached and the two board are connected through A4 and A5.
+
+  Author:
+    Shihao Piao, Ziliang Guo, Tutian Tang
+
+  Rev. 2 22 July 2019
+ */
+
+#include <LiquidCrystal.h> // to drive the LCD panel
+#include <Wire.h>          // to set up the I2C bus
 
 // initialize the library by associating any needed LCD interface pin
 // with the arduino pin number it is connected to
 LiquidCrystal lcd(2, 3, 4, 5, 6, 7, 8);
-bool to_clear = false;
+bool to_clear = false; // initialize a flag to decide wether to clear the display
 
 void receiveEvent(int howMany)
 {
+  // decide whether to clear the display
   if (to_clear)
   {
     lcd.clear();         // clear the lcd display for the next calculation
     lcd.setCursor(0, 0); // set the cursor to top-left
-    to_clear = false;
+    to_clear = false;    // once cleared, initialize the flag
   }
+
+  // cope with the byte from the I2C Bus one by one
   for (int i = 0; i < howMany; i++)
   {
     char c = Wire.read(); // receive byte as a character
+    // '.' is a flag to clear the display
     if ('.' == c)
     {
-      to_clear = true;
-      break;
+      to_clear = true; // set up the flag
+      break;           // waiting for the master for the next message
     }
+
+    // other wise, print whatever the master sends
     lcd.write(c);
+
+    // if there is an '='
     if ('=' == c)
     {
-      lcd.setCursor(0, 1);
+      lcd.setCursor(0, 1); // set the cursor to the second line
     }
   }
 }
@@ -40,5 +62,5 @@ void setup()
 
 void loop()
 {
-  delay(100);
+  delay(100); // the board should have a theoretical refresh rate of 10 Hz
 }
