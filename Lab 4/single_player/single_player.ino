a34f286a5e5da2fdd8b9fb76b93fbd48f7c6890a
marridG <1687450542@qq.com>
2019-08-09 13:42:20 -0700

    Simple end-game operation

--- Lab 4/single_player/single_player.ino
+++ Lab 4/single_player/single_player.ino
@@ -1,102 +1,114 @@
 #include <LiquidCrystal.h>
 LiquidCrystal lcd(2, 3, 4, 5, 6, 7, 8);
 
-byte heart[8] = {
-    B00000,
-    B00110,
-    B01111,
-    B11110,
-    B01111,
-    B00110,
-    B00000,
-    B00000,
-};
-byte alien[8] = {
-    B00000,
-    B10111,
-    B11101,
-    B01111,
-    B11101,
-    B10111,
-    B00000,
-    B00000,
-};
-byte plane[8] = {
-    B01000,
-    B11100,
-    B10000,
-    B11111,
-    B10000,
-    B11100,
-    B01000,
-    B00000,
-};
-byte star[8] = {
-    B00000,
-    B10010,
-    B01110,
-    B01111,
-    B01110,
-    B10010,
-    B00000,
-    B00000,
-};
-byte bomb[8] = {
-    B00000,
-    B01000,
-    B11100,
-    B11110,
-    B11101,
-    B01001,
-    B00000,
-    B00000,
-};
-byte zero[8] = {
-    B00000,
-    B00000,
-    B01110,
-    B10001,
-    B10001,
-    B01110,
-    B00000,
-    B00000,
-};
-byte one[8] = {
-    B00000,
-    B00000,
-    B10010,
-    B11111,
-    B10000,
-    B00000,
-    B00000,
-    B00000,
-};
-byte two[8] = {
-    B00000,
-    B00000,
-    B11010,
-    B10101,
-    B10010,
-    B00000,
-    B00000,
-    B00000,
-};
-
+// ---------  [START] global variables  ---------
 bool debug_mode = true;
 
 // the pins
-const int BUTTON = 9;   // D9
+const int BUTTON = 8;   // D9
 const int ROTATION = 1; // A1
 const int TILT = 11;    // D11
 
 // bool button_press = false;
-
 int board[14] = {0};
 int message[3] = {0, 0, 0};
 int life = 2;
 int skill = 2;
+// Custom Icons
+{
+    byte heart[8] =
+    {
+        B00000,
+        B00110,
+        B01111,
+        B11110,
+        B01111,
+        B00110,
+        B00000,
+        B00000,
+    };
+    byte alien[8] =
+    {
+        B00000,
+        B10111,
+        B11101,
+        B01111,
+        B11101,
+        B10111,
+        B00000,
+        B00000,
+    };
+    byte plane[8] =
+    {
+        B01000,
+        B11100,
+        B10000,
+        B11111,
+        B10000,
+        B11100,
+        B01000,
+        B00000,
+    };
+    byte star[8] =
+    {
+        B00000,
+        B10010,
+        B01110,
+        B01111,
+        B01110,
+        B10010,
+        B00000,
+        B00000,
+    };
+    byte bomb[8] =
+    {
+        B00000,
+        B01000,
+        B11100,
+        B11110,
+        B11101,
+        B01001,
+        B00000,
+        B00000,
+    };
+    byte zero[8] =
+    {
+        B00000,
+        B00000,
+        B01110,
+        B10001,
+        B10001,
+        B01110,
+        B00000,
+        B00000,
+    };
+    byte one[8] =
+    {
+        B00000,
+        B00000,
+        B10010,
+        B11111,
+        B10000,
+        B00000,
+        B00000,
+        B00000,
+    };
+    byte two[8] =
+    {
+        B00000,
+        B00000,
+        B11010,
+        B10101,
+        B10010,
+        B00000,
+        B00000,
+        B00000,
+    };
+}
+// ---------  [END] global variables  ---------
 
-// function list
+
+// ---------  [START] function list  ---------
 void initiate_game();
 // void 	button_blink();
 bool button_pressed();
@@ -107,8 +119,14 @@ void move_board_elements();
 void skill_launched();
 
 void update_board_except_player();
+void update_psn();
+
 int judge();
 void get_input();
+
+void display();
+// ---------  [END] function list  ---------
+
 
 void setup()
 {
@@ -143,9 +161,12 @@ void loop()
     if (count % 8 == 0)
         update_board_except_player();
 
-    game_status = judge();
-
     display();
+
+    game_status = judge();
+	if(!game_status)
+		while(1)
+			delay(1000);
 
     delay(100);
 
@@ -163,6 +184,8 @@ void loop()
     }
 }
 
+
+// ============ Function Implementations ============
 void initiate_game()
 {
     int i = 0;
